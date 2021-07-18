730fa663dfcbf2dee4143dd4f522a9f40f2d2342
Carlos Antonio Ceballos Romero <ceballosromerocarlos@gmail.com>
2021-07-18 18:02:36 +0200

    deploy.sh

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 # añadir tantas líneas como sean necesarias para el correcto despligue
-ansible-playbook -i hosts -l XXXX playbook
+ansible-playbook -i hosts -l 00-requirements.yaml
+ansible-playbook -i hosts -l 01-install_NFS.yaml
+ansible-playbook -i hosts -l 02-Kubernetes_workers_master.yaml
+ansible-playbook -i hosts -l 03-Kubernetes_master.yaml
+ansible-playbook -i hosts -l 04-Kubernetes_workers.yaml
+ansible-playbook -i hosts -l 05-Nginx.yaml
+
 
